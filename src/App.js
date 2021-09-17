7945f57aed2772a05514734dcafc76c92e5fd83e
BhupathiVenkataSaiCharan <77479689+BhupathiVenkataSaiCharan@users.noreply.github.com>
2021-09-17 14:24:16 +0530

    image

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <h5>GIT Host</h5>
         <p>Learnig to host react-app using github</p>
+        <img src="" alt="pic"/>
       </header>
     </div>
   );
